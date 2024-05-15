981f4ac4174d7825194a132ec9f4289688018bad
Ukasha-hub <ukashajljl@gmail.com>
2024-05-15 16:52:43 +0600

    deployment

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ require('dotenv').config()
 //middle ware
 
 app.use(cors({
-  origin:['http://localhost:5173'],
+  origin:['http://localhost:5173',
+'https://job-hunt-project-e25e2.web.app',
+'https://job-hunt-project-e25e2.firebaseapp.com'],
   credentials: true
 }))
 app.use(express.json())
@@ -37,13 +39,13 @@ const logger=(req, res, next)=>{
 
 const verifyToken= (req, res, next)=>{
   const token = req?.cookies?.token
-  //console.log('token in middleware', token)
+  console.log('token in middleware', token)
   if(!token){
-    return res.status(401).send({message: 'unauthorized access'})
+    return res.status(401).send({message: 'unauthorized access, no token'})
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
     if(err){
-      return res.status(401).send({message: 'unauthorized access'})
+      return res.status(401).send({message: 'unauthorized access, not verified'})
     }
     req.user= decoded
     next()
@@ -51,12 +53,17 @@ const verifyToken= (req, res, next)=>{
 
 }
 
+const cookieOption= {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production"? true : false,
+  sameSite: process.env.NODE_ENV === "production"? "none" : "strict",
+}
 
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
     const jobCollection=  client.db('jobHuntDB').collection('jobs')
     const appliedCollection=  client.db('jobHuntDB').collection('applied')
     
@@ -64,17 +71,13 @@ async function run() {
       const user =req.body
       console.log('user for token', user)
       const token= jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
-      res.cookie('token', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'none',
-      })
+      res.cookie('token', token, cookieOption)
       res.send({success: true})
     })
 
     app.post('/logout', async(req,res)=>{
       const user= req.body
-      res.clearCookie('token', {maxAge: 0}).send({success: true})
+      res.clearCookie('token', {...cookieOption, maxAge:0}).send({success: true})
     })
 
     app.get('/jobs', async(req,res)=>{
@@ -224,7 +227,7 @@ async function run() {
         res.send(result)
       })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    //await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
